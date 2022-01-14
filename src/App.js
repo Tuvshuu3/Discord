b531aa682c08eed2b052fb3dd6443e1d12d03ec0
Tuvshintugs Erdene <tuvshintugs@iMac2021-03s-iMac.local>
2022-01-14 18:19:04 +0800

    sonordumbaf

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import { Container, Navbar, Button, Nav,  } from "react-bootstrap";
 import LogoNname from "./assets/LogoNname.svg"
 import Background2 from "./assets/Background2.svg"
 import Background3 from "./assets/Background3.svg"
-import DownloadIcon from '@mui/icons-material/Download';
-
-
+import 'material-icons/iconfont/material-icons.css';
 
 const App = () => {
   return (
@@ -17,7 +15,7 @@ const App = () => {
             <Navbar.Brand style={{'width': '140px', 'height': '46px'}} href="#home"><img src={LogoNname} /></Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto w-88 justify-content-center gap-3">
+              <Nav className="me-auto w-88 justify-content-center gap-4">
                 <Nav.Link className='white header-style'>Download</Nav.Link>
                 <Nav.Link className='white header-style'>Nitro</Nav.Link>
                 <Nav.Link className='white header-style'>Safety</Nav.Link>
@@ -27,21 +25,24 @@ const App = () => {
               </Nav>
             </Navbar.Collapse>
             <Nav>
-              <Button className='Literally_Everything-First-Navbar-Button' style={{"border-radius": "100px", "width": "64px", "height": "38px", "font-size": "14px", "fontWeight": "500"}} variant="light">Login</Button>
+              <Button className='Literally_Everything-First-Navbar-Button' variant="light">Login</Button>
               </Nav>
           </Container>
         </Navbar>
 
         <div className='Literally_Everything-First-Middle d-flex justify-content-center align-items-center'>
           <div className='Literally_Everything-First-Middle-stuff d-flex flex-column'>
-            <div className='Literally_Everything-First-Middle-stuff-Text text-align-center'>
-              <div className='Literally_Everything-First-Middle-stuff-Text-Big white'>IMAGINE A PLACE...</div>
+            <div className='Literally_Everything-First-Middle-stuff-Text'>
+              <div className='Literally_Everything-First-Middle-stuff-Text-Big text-center white'>IMAGINE A PLACE...</div>
               <div className='Literally_Everything-First-Middle-stuff-Text-Small white'>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</div>
             </div>
    
-            <div className='Literally_Everything-First-Middle-stuff-Buttons'>
-            <Button href="//discord.com/api/download?platform=osx" className='Literally_Everything-First-Middle-stuff-Buttons-Download' style={{"border-radius": "100px", "width": "294px", "height": "56px", "font-size": "20px", "fontWeight": "500"}} variant="light"> {DownloadIcon} Download for Mac</Button>
-            <Button className='Literally_Everything-First-Navbar-Button' style={{"border-radius": "100px", "width": "315px", "height": "56px", "font-size": "20px", "fontWeight": "500"}} variant="light">Open Discord in your browser</Button>
+            <div className='Literally_Everything-First-Middle-stuff-Buttons d-flex flex-row justify-content-center'>
+            <Button href="//discord.com/api/download?platform=osx"  className='Literally_Everything-First-Middle-stuff-Buttons-Download d-flex flex-row justify-content-evenly align-items-center' variant="light">
+              <span class="material-icons">download</span>
+              Download for Mac
+            </Button>
+            <Button className='Literally_Everything-First-Middle-stuff-Buttons-Open black' variant="dark">Open Discord in your browser</Button>
             </div>
 
           </div>
