01b32b6c0edd4b5451ba228597fad8a15066cb75
Tuvshuu3 <tuvshintugserdenee12@gmail.com>
2022-01-19 12:39:52 +0800

    ok

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,17 @@
-import "./styles/index.scss";
+import "../styles/index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";          
 import { Container, Navbar, Button, Nav, Carousel, Dropdown  } from "react-bootstrap";
-import LogoNname from "./assets/LogoNname.svg"
-import Background2 from "./assets/Background2.svg"
-import Background3 from "./assets/Background3.svg"
-import Carousel1 from './assets/Carousel1.png'
-import Carousel2 from './assets/Carousel2.png'
-import Carousel3 from './assets/Carousel3.png'
+import LogoNname from "../assets/LogoNname.svg"
+import Background2 from "../assets/Background2.svg"
+import Background3 from "../assets/Background3.svg"
+import Carousel1 from '../assets/Carousel1.png'
+import Carousel2 from '../assets/Carousel2.png'
+import Carousel3 from '../assets/Carousel3.png'
 import 'material-icons/iconfont/material-icons.css';
-import SocialMedia from './assets/Socialmedia.png'
+import SocialMedia from '../assets/Socialmedia.png'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Login from './components/Login'
-import Root from './App.js'
 
-<Router>
-  <Routes>
-    <Route exact path="/" element={<Root/>}/>
-    <Route path="/login" element={<Login/>}/>
-  </Routes>
-</Router>
-
-const App = () => {
+const Home = () => {
   return (
     <div className="Literally_Everything">
       <div className="Literally_Everything-First d-flex flex-column align-items-center w-100 justify-content-between">
@@ -158,4 +149,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+    return(
+        <div className="">
+            HI THIS IS LOGIN YOU DUMBASS
+        </div>
+    )
+}
+
+export default Login;
