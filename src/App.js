28581dd4f40197a8ba35d99950a1a2a9afe04c8d
Tuvshintugs Erdene <tuvshintugs@iMac2021-03s-iMac.local>
2022-02-07 17:56:57 +0800

    yeah boiiiii

--- src/App.js
+++ src/App.js
@@ -11,151 +11,17 @@ import 'material-icons/iconfont/material-icons.css';
 import SocialMedia from './assets/Socialmedia.png'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from './components/Login'
-import Root from './App.js'
+import Home from './components/Home.js'
 
-<Router>
-  <Routes>
-    <Route exact path="/" element={<Root/>}/>
-    <Route path="/login" element={<Login/>}/>
-  </Routes>
-</Router>
 
 const App = () => {
-  return (
-    <div className="Literally_Everything">
-      <div className="Literally_Everything-First d-flex flex-column align-items-center w-100 justify-content-between">
-        <Navbar className='Literally_Everything-First-Navbar' collapseOnSelect expand="lg" bg="#primary" variant="dark">
-          <Container>
-            <Navbar.Brand style={{'width': '140px', 'height': '46px'}} href="#home"><img src={LogoNname} /></Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto w-88 justify-content-center gap-4">
-                <Nav.Link className='white header-style'>Download</Nav.Link>
-                <Nav.Link className='white header-style'>Nitro</Nav.Link>
-                <Nav.Link className='white header-style'>Safety</Nav.Link>
-                <Nav.Link className='white header-style'>Support</Nav.Link>
-                <Nav.Link className='white header-style'>Blog</Nav.Link>
-                <Nav.Link className='white header-style'>Careers</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-            <Nav>
-              <Link to='/login'>
-                <Button className='Literally_Everything-First-Navbar-Button' variant="light">Login</Button>
-              </Link>
-              </Nav>
-          </Container>
-        </Navbar>
-
-        <div className='Literally_Everything-First-Middle d-flex justify-content-center align-items-center'>
-          <div className='Literally_Everything-First-Middle-stuff d-flex flex-column'>
-            <div className='Literally_Everything-First-Middle-stuff-Text'>
-              <div className='Literally_Everything-First-Middle-stuff-Text-Big text-center white'>IMAGINE A PLACE...</div>
-              <div className='Literally_Everything-First-Middle-stuff-Text-Small white'>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</div>
-            </div>
-   
-            <div className='Literally_Everything-First-Middle-stuff-Buttons d-flex flex-row justify-content-center'>
-            <Button href="//discord.com/api/download?platform=osx"  className='Literally_Everything-First-Middle-stuff-Buttons-Download d-flex flex-row justify-content-evenly align-items-center' variant="light">
-              <span class="material-icons">download</span>
-              Download for Mac
-            </Button>
-            <Button className='Literally_Everything-First-Middle-stuff-Buttons-Open black' variant="dark">Open Discord in your browser</Button>
-            </div>
-          </div>
-        </div>
-        
-        {/* <div className='img1-2 d-flex flex-row justify-content-between'>
-            <img src={Background2} className='Literally_Everything-First-Background2' width={700}/>
-            <img src={Background3} className='Literally_Everything-First-Background3' width={610}/>
-        </div> */}
-      </div>
-
-      <div className="carousel-stuff">
-        <Carousel>
-          <Carousel.Item>
-            <img className="d-block w-100" src={Carousel1} alt="First slide"/>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block w-100" src={Carousel2} alt="Second slide"/>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block w-100" src={Carousel3} alt="Third slide"/>  
-          </Carousel.Item>
-        </Carousel>
-      </div>
-
-      <div className="FooterContainer">
-        <div className="Footer">
-          <div className="Footer-TopPart">
-            <div className="Footer-TopPart-SocialMedia">
-              <div className="Footer-TopPart-SocialMedia-Imagine">IMAGINE A PLACE</div>
-              <>
-              <Dropdown>
-                <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-                  English, USA
-                </Dropdown.Toggle>
-                <Dropdown.Menu variant="dark">
-                  <Dropdown.Item href="#/action-1" active>
-                  English, USA
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">Ydaj 90 </Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">ugchih</Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href="#/action-4">please</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-              </>
-              <div className="Footer-TopPart-SocialMedia-image">
-                <img src={SocialMedia}></img>
-              </div>
-            </div>
-            <div className="Footer-TopPart-ALot">
-              <div className="Footer-TopPart-ALot-component">
-                <div className="Footer-TopPart-ALot-component-title">Product</div>
-                <div className="Footer-TopPart-ALot-component-content">Download</div>
-                <div className="Footer-TopPart-ALot-component-content">Nitro</div>
-                <div className="Footer-TopPart-ALot-component-content">Status</div>
-              </div>
-
-              <div className="Footer-TopPart-ALot-component">
-                <div className="Footer-TopPart-ALot-component-title">Company</div>
-                <div className="Footer-TopPart-ALot-component-content">About</div>
-                <div className="Footer-TopPart-ALot-component-content">Jobs</div>
-                <div className="Footer-TopPart-ALot-component-content">Branding</div>
-                <div className="Footer-TopPart-ALot-component-content">Newsroom</div>
-              </div>
-
-              <div className="Footer-TopPart-ALot-component">
-                <div className="Footer-TopPart-ALot-component-title">Resources</div>
-                <div className="Footer-TopPart-ALot-component-content">College</div>
-                <div className="Footer-TopPart-ALot-component-content">Support</div>
-                <div className="Footer-TopPart-ALot-component-content">Safety</div>
-                <div className="Footer-TopPart-ALot-component-content">Blog</div>
-                <div className="Footer-TopPart-ALot-component-content">Feedback</div>
-                <div className="Footer-TopPart-ALot-component-content">Developers</div>
-                <div className="Footer-TopPart-ALot-component-content">StreamKit</div>
-              </div>
-
-              <div className="Footer-TopPart-ALot-component">
-                <div className="Footer-TopPart-ALot-component-title">Policies</div>
-                <div className="Footer-TopPart-ALot-component-content">Terms</div>
-                <div className="Footer-TopPart-ALot-component-content">Privacy</div>
-                <div className="Footer-TopPart-ALot-component-content">Cookie Settings</div>
-                <div className="Footer-TopPart-ALot-component-content">Guidelines</div>
-                <div className="Footer-TopPart-ALot-component-content">Acknowledgements</div>
-                <div className="Footer-TopPart-ALot-component-content">Licenses</div>
-                <div className="Footer-TopPart-ALot-component-content">Moderation</div>
-              </div>
-            </div>
-          </div>
-    
-          <div className="Footer-BottomPart">
-            <img src={LogoNname}></img>
-            <Button className='Footer-BottomPart-Button' variant="dark">Open Discord</Button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return(
+  <Router>
+  <Routes>
+    <Route exact path="/" element={<Home/>}/>
+    <Route path="/login" element={<Login/>}/>
+  </Routes>
+</Router>)
 };
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,20 @@
 import React from "react";
+import loginBck from '../assets/login.png'
+import '../styles/index.scss'
 
 const Login = () => {
     return(
-        <div className="">
-            HI THIS IS LOGIN YOU DUMBASS
+        <div className='Literally_Everything-loginBG'>
+            <img style={{width: '100vw', height: '100vh'}} src={loginBck}/>
+            <div className='greysh'>
+                <div className='greysh-leftside'>
+
+                </div>
+
+                <div className='greysh-rightside'>
+                    
+                </div>
+            </div>
         </div>
     )
 }
